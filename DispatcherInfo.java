3eaf7131723c61d210d202f033803438c29617f6
ideaflyit <43774426+ideaflyit@users.noreply.github.com>
2020-03-15 13:37:11 +0800

    Add files via upload
    add DispatcherInfo.java

---
+++ DispatcherInfo.java
@@ -0,0 +1,40 @@
+/**
+ * Project Name:live-project
+
+ * File Name:DispatcherInfo.java
+ * Package Name:
+ * Date:обнГ1:01:16
+ * Copyright (c) 2020, Doris All Rights Reserved.
+ *
+*/
+
+/**
+ * Description: <br/>
+ * Date: обнГ1:01:16 <br/>
+ * 
+ * @author Doris
+ * @version
+ * @see
+ */
+public class DispatcherInfo {
+    int dispatcherId;
+
+    boolean dispatcher;
+
+    public int getDispatcherId() {
+        return dispatcherId;
+    }
+
+    public void setDispatcherId(int dispatcherId) {
+        this.dispatcherId = dispatcherId;
+    }
+
+    public boolean isDispatcher() {
+        return dispatcher;
+    }
+
+    public void setDispatcher(boolean dispatcher) {
+        this.dispatcher = dispatcher;
+    }
+
+}
