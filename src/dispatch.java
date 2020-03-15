7887264a2126fc665c57ec0ab7c108b15011e881
PearInJurassic <aassd123lowe@outlook.com>
2020-03-15 14:16:40 +0800

    前端跳转基本实现

--- src/dispatch.java
+++ src/dispatch.java
@@ -1,6 +1,25 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class dispatch {
+    public dispatch() {
+        SearchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //在数据库中搜索是否中签
+                //if中签，弹出对话框确定信息
+                mssage dialog = new mssage();
+                dialog.pack();
+                dialog.setVisible(true);
+                System.exit(0);
+                //inputTextField.setText("done");
+                //else
+                JOptionPane.showMessageDialog(null,"抱歉您并未中签");
+            }
+        });
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("dispatch");
         frame.setContentPane(new dispatch().panel1);
@@ -10,8 +29,6 @@ public class dispatch {
     }
 
     private JPanel panel1;
+    private JTextField inputTextField;
     private JButton SearchButton;
-    private JTextField textField1;
-    private JLabel Title;
-    private JButton CancelButton;
 }
