6048f186d143f744d2dee1d1f814641e77cabc46
yanhar <30743657+yanhar@users.noreply.github.com>
2020-03-15 16:06:12 +0800

    增加判断登记

---
+++ src/com/company/BookInfo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class BookInfo {
+    String name;
+    String id;
+    String phone;
+    int maskNum;
+
+    BookInfo(String name,String id, String phone,int maskNum){
+        this.name=name;
+        this.id=id;
+        this.phone=phone;
+        this.maskNum=maskNum;
+    }
+}
